7daac10e18ed170f4bd8e5a0327538abcc9a9737
Jia He <heyingjia1988@gmail.com>
2025-09-23 22:02:01 +0200

    modify index.html main.js .gitignore

--- quiz-app/src/js/main.js
+++ quiz-app/src/js/main.js
@@ -3,6 +3,8 @@ import "../sass/main.scss";
 import { setColorMode, checkModeChange } from "./theme.js";
 import quizData from "../data/data.json";
 
+const iconCorrect = "/images/icon-correct.svg";
+const iconIncorrect = "/images/icon-incorrect.svg";
 // Initial setup for theme
 setColorMode();
 checkModeChange();
@@ -142,7 +144,7 @@ submitBtn.addEventListener("click", () => {
     selectedBtn.insertAdjacentHTML(
       "beforeend",
       ` <span class="result-icon">
-          <img src="public/images/icon-correct.svg" alt="Correct icon" />
+          <img src="/images/icon-correct.svg" alt="Correct icon" />
         </span>`
     );
 
@@ -153,7 +155,7 @@ submitBtn.addEventListener("click", () => {
     selectedBtn.insertAdjacentHTML(
       "beforeend",
       ` <span class="result-icon">
-          <img src="public/images/icon-incorrect.svg" alt="Correct icon" />
+          <img src="/images/icon-incorrect.svg" alt="Correct icon" />
         </span>`
     );
   }
